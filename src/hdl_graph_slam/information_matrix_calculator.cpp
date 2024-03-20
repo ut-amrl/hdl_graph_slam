bdc69a3df4192d43b4a8ebbb12d7845b751f04f4
Dongmyeong Lee <domlee@utexas.edu>
2024-03-20 15:11:55 -0500

    reformatting

--- src/hdl_graph_slam/information_matrix_calculator.cpp
+++ src/hdl_graph_slam/information_matrix_calculator.cpp
@@ -2,12 +2,12 @@
 
 #include <hdl_graph_slam/information_matrix_calculator.hpp>
 
-#include <pcl/search/kdtree.h>
 #include <pcl/common/transforms.h>
+#include <pcl/search/kdtree.h>
 
 namespace hdl_graph_slam {
 
-InformationMatrixCalculator::InformationMatrixCalculator(ros::NodeHandle& nh) {
+InformationMatrixCalculator::InformationMatrixCalculator(ros::NodeHandle &nh) {
   use_const_inf_matrix = nh.param<bool>("use_const_inf_matrix", false);
   const_stddev_x = nh.param<double>("const_stddev_x", 0.5);
   const_stddev_q = nh.param<double>("const_stddev_q", 0.1);
@@ -22,8 +22,11 @@ InformationMatrixCalculator::InformationMatrixCalculator(ros::NodeHandle& nh) {
 
 InformationMatrixCalculator::~InformationMatrixCalculator() {}
 
-Eigen::MatrixXd InformationMatrixCalculator::calc_information_matrix(const pcl::PointCloud<PointT>::ConstPtr& cloud1, const pcl::PointCloud<PointT>::ConstPtr& cloud2, const Eigen::Isometry3d& relpose) const {
-  if(use_const_inf_matrix) {
+Eigen::MatrixXd InformationMatrixCalculator::calc_information_matrix(
+    const pcl::PointCloud<PointT>::ConstPtr &cloud1,
+    const pcl::PointCloud<PointT>::ConstPtr &cloud2,
+    const Eigen::Isometry3d &relpose) const {
+  if (use_const_inf_matrix) {
     Eigen::MatrixXd inf = Eigen::MatrixXd::Identity(6, 6);
     inf.topLeftCorner(3, 3).array() /= const_stddev_x;
     inf.bottomRightCorner(3, 3).array() /= const_stddev_q;
@@ -37,8 +40,10 @@ Eigen::MatrixXd InformationMatrixCalculator::calc_information_matrix(const pcl::
   double min_var_q = std::pow(min_stddev_q, 2);
   double max_var_q = std::pow(max_stddev_q, 2);
 
-  float w_x = weight(var_gain_a, fitness_score_thresh, min_var_x, max_var_x, fitness_score);
-  float w_q = weight(var_gain_a, fitness_score_thresh, min_var_q, max_var_q, fitness_score);
+  float w_x =
+      weight(var_gain_a, fitness_score_thresh, min_var_x, max_var_x, fitness_score);
+  float w_q =
+      weight(var_gain_a, fitness_score_thresh, min_var_q, max_var_q, fitness_score);
 
   Eigen::MatrixXd inf = Eigen::MatrixXd::Identity(6, 6);
   inf.topLeftCorner(3, 3).array() /= w_x;
@@ -46,7 +51,10 @@ Eigen::MatrixXd InformationMatrixCalculator::calc_information_matrix(const pcl::
   return inf;
 }
 
-double InformationMatrixCalculator::calc_fitness_score(const pcl::PointCloud<PointT>::ConstPtr& cloud1, const pcl::PointCloud<PointT>::ConstPtr& cloud2, const Eigen::Isometry3d& relpose, double max_range) {
+double InformationMatrixCalculator::calc_fitness_score(
+    const pcl::PointCloud<PointT>::ConstPtr &cloud1,
+    const pcl::PointCloud<PointT>::ConstPtr &cloud2, const Eigen::Isometry3d &relpose,
+    double max_range) {
   pcl::search::KdTree<PointT>::Ptr tree_(new pcl::search::KdTree<PointT>());
   tree_->setInputCloud(cloud1);
 
@@ -61,22 +69,22 @@ double InformationMatrixCalculator::calc_fitness_score(const pcl::PointCloud<Poi
 
   // For each point in the source dataset
   int nr = 0;
-  for(size_t i = 0; i < input_transformed.points.size(); ++i) {
+  for (size_t i = 0; i < input_transformed.points.size(); ++i) {
     // Find its nearest neighbor in the target
     tree_->nearestKSearch(input_transformed.points[i], 1, nn_indices, nn_dists);
 
     // Deal with occlusions (incomplete targets)
-    if(nn_dists[0] <= max_range) {
+    if (nn_dists[0] <= max_range) {
       // Add to the fitness score
       fitness_score += nn_dists[0];
       nr++;
     }
   }
 
-  if(nr > 0)
+  if (nr > 0)
     return (fitness_score / nr);
   else
     return (std::numeric_limits<double>::max());
 }
 
-}  // namespace hdl_graph_slam
+} // namespace hdl_graph_slam
